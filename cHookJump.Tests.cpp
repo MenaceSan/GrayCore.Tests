f66a6e7a20620eef0af05fed0bc579ac3d569332
Dennis <github@menasoft.com>
2021-02-19 16:50:47 -0500

    update hook tests

--- cHookJump.Tests.cpp
+++ cHookJump.Tests.cpp
@@ -6,29 +6,105 @@
 
 namespace Gray
 {
-	// Make sure this code is not optimized out !
+	static cHookJump sm_cHookJump_HookAPI;
+
+	UNITTEST2_CLASS(cHookJump)
+	{
+		// Make sure this code is not optimized out !
 #ifdef _MSC_VER
 #pragma optimize( "", off )
 #endif
-	INT_PTR GRAYCALL cUnitTest_HookJump1() // never inline optimize this
-	{
-		cUnitTests& uts = cUnitTests::I();
-		uts.m_pLog->addDebugInfoF("cUnitTest_HookJump1");
-		return 1;
-	}
-	INT_PTR GRAYCALL cUnitTest_HookJump2() // never inline optimize this
-	{
-		// replace cUnitTest_HookJump1 with this.
-		cUnitTests& uts = cUnitTests::I();
-		uts.m_pLog->addDebugInfoF("cUnitTest_HookJump2");
-		return 2;
-	}
+		static INT_PTR GRAYCALL HookJump1() // never inline optimize this
+		{
+			cUnitTests& uts = cUnitTests::I();
+			uts.m_pLog->addDebugInfoF("cUnitTest_HookJump1");
+			return 1;
+		}
+		static INT_PTR GRAYCALL HookJump2() // never inline optimize this
+		{
+			// replace cUnitTest_HookJump1 with this.
+			cUnitTests& uts = cUnitTests::I();
+			uts.m_pLog->addDebugInfoF("cUnitTest_HookJump2");
+			return 2;
+		}
+		static void GRAYCALL HookAPI(const char* psz) // never inline optimize this
+		{
+			// sm_HookAPI. replace OutputDebugString() with this.
+
+			cHookSwapLock lock(sm_cHookJump_HookAPI);
+			::OutputDebugStringA("PREFIX WITH CUSTOM:");	// raw call.
+			::OutputDebugStringA(psz);	// raw call.
+		}
+
 #ifdef _MSC_VER
 #pragma optimize( "", on )	// restore old params.
 #endif
 
-	UNITTEST2_CLASS(cHookJump)
-	{
+		void TestBasic()
+		{
+			// Test replacing a internal call.
+			INT_PTR iRet = HookJump1();	// test with no hooks.
+			UNITTEST2_TRUE(iRet == 1);
+
+			cHookJump tester;
+			HRESULT hRes = tester.InstallHook((FARPROC)HookJump1, (FARPROC)HookJump2);
+			UNITTEST2_TRUE(tester.isHookInstalled());
+
+			iRet = HookJump1();	// really calls cUnitTest_HookJump2
+			UNITTEST2_TRUE(iRet == 2);
+
+			iRet = HookJump2();
+			UNITTEST2_TRUE(iRet == 2);
+
+			{
+				cHookSwapLock lock(tester);
+				iRet = HookJump1();	// cUnitTest_HookJump1 was restored globally.
+				UNITTEST2_TRUE(iRet == 1);
+			}
+
+			iRet = HookJump1();
+			UNITTEST2_TRUE(iRet == 2);
+
+			{
+				cHookSwapChain<> lock(tester);
+				iRet = HookJump1();	// cUnitTest_HookJump1 still as cUnitTest_HookJump2.
+				UNITTEST2_TRUE(iRet == 2);
+				iRet = lock.m_pFuncChain();	// cUnitTest_HookJump1 was restored just for Chain.
+				UNITTEST2_TRUE(iRet == 1);
+			}
+
+			iRet = HookJump1();	// cUnitTest_HookJump1 as cUnitTest_HookJump2.
+			UNITTEST2_TRUE(iRet == 2);
+
+			tester.RemoveHook();
+			UNITTEST2_TRUE(!tester.isHookInstalled());
+
+			iRet = HookJump1();
+			UNITTEST2_TRUE(iRet == 1);	// was restored.
+			iRet = HookJump2();
+			UNITTEST2_TRUE(iRet == 2);		// still works as expected.
+		}
+
+		typedef VOID(FAR WINAPI* OutputDebugStringA_t)(_In_opt_ LPCSTR lpOutputString);
+
+		void TestAPI()
+		{
+			// Test replacing a call i pull from a system DLL.
+			// sm_HookAPI
+
+			OutputDebugStringA_t pFuncOrig = ::OutputDebugStringA;
+
+			::OutputDebugStringA("TestAPI() 1 (No prefix)\n");	// raw call.
+
+			HRESULT hRes = sm_cHookJump_HookAPI.InstallHook((FARPROC)pFuncOrig, (FARPROC)HookAPI, true);
+
+			::OutputDebugStringA("TestAPI() call\n");	// raw call.
+
+			sm_cHookJump_HookAPI.RemoveHook();
+
+			::OutputDebugStringA("TestAPI() 2 (No prefix)\n");	// raw call.
+		}
+
 		UNITTEST2_METHOD(cHookJump)
 		{
 			//! hook a API function for one call.
@@ -36,46 +112,8 @@ namespace Gray
 			cUnitTests& uts = cUnitTests::I();
 			UNITTEST2_TRUE(uts.m_pLog != nullptr);
 
-			INT_PTR iRet = cUnitTest_HookJump1();
-			UNITTEST2_TRUE(iRet == 1);
-
-			cHookJump tester;
-			tester.InstallHook((FARPROC)cUnitTest_HookJump1, (FARPROC)cUnitTest_HookJump2);
-			UNITTEST2_TRUE(tester.isHookInstalled());
-
-			iRet = cUnitTest_HookJump1();	// really calls cUnitTest_HookJump2
-			UNITTEST2_TRUE(iRet == 2);
-
-			iRet = cUnitTest_HookJump2();
-			UNITTEST2_TRUE(iRet == 2);
-
-			{
-				cHookSwapLock lock(tester);
-				iRet = cUnitTest_HookJump1();	// cUnitTest_HookJump1 was restored globally.
-				UNITTEST2_TRUE(iRet == 1);
-			}
-
-			iRet = cUnitTest_HookJump1();
-			UNITTEST2_TRUE(iRet == 2);
-
-			{
-				cHookSwapChain lock(tester);
-				iRet = cUnitTest_HookJump1();	// cUnitTest_HookJump1 still as cUnitTest_HookJump2.
-				UNITTEST2_TRUE(iRet == 2);
-				iRet = lock.m_pFuncChain();	// cUnitTest_HookJump1 was restored just for Chain.
-				UNITTEST2_TRUE(iRet == 1);
-			}
-
-			iRet = cUnitTest_HookJump1();	// cUnitTest_HookJump1 as cUnitTest_HookJump2.
-			UNITTEST2_TRUE(iRet == 2);
-
-			tester.RemoveHook();
-			UNITTEST2_TRUE(!tester.isHookInstalled());
-
-			iRet = cUnitTest_HookJump1();
-			UNITTEST2_TRUE(iRet == 1);	// was restored.
-			iRet = cUnitTest_HookJump2();
-			UNITTEST2_TRUE(iRet == 2);		// still works as expected.
+			TestBasic();
+			TestAPI();
 		}
 	};
 	UNITTEST2_REGISTER(cHookJump, UNITTEST_LEVEL_Core);
